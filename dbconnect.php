83aefba59a568db8ed27012b96d3d4c004ed8fd1
sudhirr4u <sudhirr4u@gmail.com>
2017-09-27 14:44:03 +0530

    sdfsdf

--- dbconnect.php
+++ dbconnect.php
@@ -2,8 +2,8 @@
 
 $db_host = "13.126.202.106";
 $db_name = "itcinfo";
-$db_user = "root";
-$db_pass = "root";
+$db_user = "appuser";
+$db_pass = "appuser";
 
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
